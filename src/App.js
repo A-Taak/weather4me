38851b453f895b1e5a5f60bc802e9a23307f7c9d
AjayTaak <ajmehr.taak@gmail.com>
2023-05-16 17:47:36 +0100

    Fix: Corrected import statement.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Search from "./components/search/search";
 import {WEATHER_API_URL , WEATHER_API_KEY} from "./api";
 import CurrentWeather from "./components/current-weather/current-weather";
-import {useState} from "react";
+import React, { useState } from "react";
 import Forecast from "./components/forecast/forecast";
 
 function App() {
