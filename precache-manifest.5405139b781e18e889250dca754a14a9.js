a79061a3eff0c1f53461ab4795dda1bc4dcaadad
AjayTaak <ajmehr.taak@gmail.com>
2023-05-16 17:45:51 +0100

    Updates

---
+++ precache-manifest.5405139b781e18e889250dca754a14a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dedde46ecbe520694d0b",
+    "url": "/weather4me/static/css/main.1e03bd55.chunk.css"
+  },
+  {
+    "revision": "dedde46ecbe520694d0b",
+    "url": "/weather4me/static/js/main.dedde46e.chunk.js"
+  },
+  {
+    "revision": "5bff56686b744a6e6287",
+    "url": "/weather4me/static/js/1.5bff5668.chunk.js"
+  },
+  {
+    "revision": "48ce93989461e37093af",
+    "url": "/weather4me/static/js/2.48ce9398.chunk.js"
+  },
+  {
+    "revision": "28470487ca56b063adb1",
+    "url": "/weather4me/static/js/runtime~main.28470487.js"
+  },
+  {
+    "revision": "0931926a4c3bee5f629388286b5f2d57",
+    "url": "/weather4me/index.html"
+  }
+];
